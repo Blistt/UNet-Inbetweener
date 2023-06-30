ce2b440d057a2bd74626acc405c89be6f5af6857
Blistt <farriagapazos@miners.utep.edu>
2023-06-30 16:01:23 -0600

    training and visualizaiton modulized

--- utils.py
+++ utils.py
@@ -6,18 +6,7 @@ import numpy as np
 import os
 from torchvision import transforms
 import imageio
-
-
-"""Supporting functions"""
-def show_tensor_images(image_tensor, num_images=16, size=(1, 28, 28)):
-    '''
-    Function for visualizing images: Given a tensor of images, number of images, and
-    size per image, plots and prints the images in an uniform grid.
-    '''
-    image_unflat = image_tensor.detach().cpu()
-    image_grid = make_grid(image_unflat[:num_images], nrow=4)
-    plt.axis('off')
-    plt.imshow(image_grid.permute(1, 2, 0).squeeze())
+from PIL import Image
 
 
 def crop(image, new_shape):
@@ -31,8 +20,6 @@ def crop(image, new_shape):
     padding_y = (image.shape[-2]-new_shape[-2]) // 2
     padding_x = (image.shape[-1]-new_shape[-1]) // 2
     odd_y, odd_x = (image.shape[-2]-new_shape[-2]) % 2, (image.shape[-1]-new_shape[-1]) % 2
-    
-    # Crops whole batch or a single image
     # Crops whole batch or a single image
     if image.dim() > 3:
       cropped_image = image[:, :, padding_y+odd_y:image.shape[-2]-padding_y, padding_x+odd_x:image.shape[-1]-padding_x]
@@ -47,20 +34,68 @@ def pre_process(img, binarize_at=0.0, resize_to=(0,0)):
         # Replace all values in img that are less than the max value with 0
         img[img<thresh] = 0
         img[img>=thresh] = 255
-
     if resize_to != (0,0):
         img = cv2.resize(img, resize_to, interpolation=cv2.INTER_CUBIC)
-
     if binarize_at > 0.0:
         thresh = int(binarize_at * np.max(img))
         # Replace all values in img that are less than the max value with 0
         img[img<thresh] = 0
         img[img>=thresh] = 255
-
     return img
 
-def create_gif(triplet, filename):
-    imageio.mimsave(filename+'.gif', triplet, duration=0.5)
+def show_tensor_images(image_tensor, num_images=16, size=(1, 28, 28)):
+    '''
+    Function for visualizing images: Given a tensor of images, number of images, and
+    size per image, plots and prints the images in an uniform grid.
+    '''
+    image_unflat = image_tensor.detach().cpu()
+    image_grid = make_grid(image_unflat[:num_images], nrow=4)
+    plt.axis('off')
+    plt.imshow(image_grid.permute(1, 2, 0).squeeze())
+
+def create_gif(input1, input2, pred, labels, experiment_dir, epoch):
+    input1, input2 = crop(input1[0], pred.shape), crop(input2[0], pred.shape)
+    pred = Image.fromarray(np.squeeze((pred[0].detach().cpu().numpy() * 255).astype(np.uint8), axis=0))
+    input1 = Image.fromarray(np.squeeze((input1.detach().cpu().numpy() * 255).astype(np.uint8), axis=0))
+    input2 = Image.fromarray(np.squeeze((input2.detach().cpu().numpy() * 255).astype(np.uint8), axis=0))
+    labels = Image.fromarray(np.squeeze((labels[0].detach().cpu().numpy() * 255).astype(np.uint8), axis=0))
+    # Gif for generated triplet
+    input1.save(experiment_dir + 'triplet_true' + str(epoch) + '.gif', save_all=True, append_images=[pred, input2], duration=500, loop=0)
+    # Gif for ground truth triplet
+    input2.save(experiment_dir + 'triplet_pred' + str(epoch) + '.gif', save_all=True, append_images=[labels, input1], duration=500, loop=0)
+
+def visualize_batch(input1, labels, input2, pred, model, losses, epoch, experiment_dir, train_test='training'):
+        # Creates experiment directory if it doesn't exist
+        experiment_dir = experiment_dir + train_test + '/'
+        if not os.path.exists(experiment_dir): os.makedirs(experiment_dir)
+
+        if epoch == 0:
+            # Save snapshot of model architecture
+            with open(experiment_dir + 'model_architecture.txt', 'w') as f:
+                print(model, file=f)
+
+        # Plots losses
+        plt.figure()
+        plt.plot(losses)
+        plt.title("Loss per Epoch")
+        plt.xlabel("Epoch")
+        plt.ylabel("Loss")
+        plt.savefig(experiment_dir + 'loss' + str(epoch) + '.png')
+        
+        # Plots generated images
+        # plt.figure() # Create a new figure for the subplots
+        plt.subplot(1,2,1)
+        show_tensor_images(labels)
+        plt.title("True")
+        plt.subplot(1,2,2)
+        show_tensor_images(pred)
+        plt.title("Generated")
+        plt.savefig(experiment_dir + 'gens' + str(epoch) + '.png')
+
+        # Saves gifs of the predicted and ground truth triplets
+        create_gif(input1, input2, pred, labels, experiment_dir, epoch)
+
+
 
 # """Read Minidataset"""
 # def read_minidataset(data_dir, transform=True, pre_process=False, crop_shape=(0,0)):
